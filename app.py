888d2383ec8d31dc3f03b2e3ee0b1818e16ec884
Chris Weaver <chris0030@pm.me>
2023-07-28 08:23:33 +0100

    Add shows and images

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ import json
 with open('shows.json', 'r') as file:
     shows = json.load(file)
 
-print(shows)
+
 
 environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("index.html")
+show_template = environment.get_template("show.html")
 
 content = template.render(shows=shows)
 with open('docs/index.html', mode="w", encoding="utf-8") as message:
     message.write(content)
+
+for show in shows:
+    content = show_template.render(show=show)
+    with open(f"docs/{show['iso_date']}.html", mode="w", encoding="utf-8") as file:
+        file.write(content)
