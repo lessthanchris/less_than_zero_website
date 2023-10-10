5a18ef5b4f4576330a7b0e697e4a9ae6ed583a8f
Chris Weaver <chris0030@pm.me>
2023-10-10 07:25:02 +0100

    Prepare for 12th October show

---
+++ prepare_assets.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Preparing show assets"
+
+convert -resize 500x500 docs/images/$1.png docs/images/$1.thumbnail.png
+cwebp docs/images/$1.png -o docs/images/$1.png.webp -resize 500 0 -q 100
+python3 readable_to_json.py $1.txt
+mv $1.json tracklists/
