27b5c36c1585efc50c987407e7df9eba935c346b
Chris Weaver <chris0030@pm.me>
2023-10-20 07:58:57 +0100

    Add 19th and 12th shows

--- prepare_assets.sh
+++ prepare_assets.sh
@@ -5,4 +5,5 @@ echo "Preparing show assets"
 convert -resize 500x500 docs/images/$1.png docs/images/$1.thumbnail.png
 cwebp docs/images/$1.png -o docs/images/$1.png.webp -resize 500 0 -q 100
 python3 readable_to_json.py $1.txt
+rm $1.txt
 mv $1.json tracklists/
