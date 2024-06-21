2b92d64e1403651b838259a565eb5e2fe30e4fa7
Chris Weaver <chris0030@pm.me>
2024-06-21 07:51:27 +0100

    Add the 13th and 20th shows, plus archive

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ import json
 with open('shows.json', 'r') as file:
     shows = json.load(file)
 
-
-
 environment = Environment(loader=FileSystemLoader("templates/"), trim_blocks=True, lstrip_blocks=True)
 template = environment.get_template("index.html")
 show_template = environment.get_template("show.html")
+archive_template = environment.get_template("archive.html")
+
+ALL_SONGS = []
 
 content = template.render(shows=shows)
 with open('docs/index.html', mode="w", encoding="utf-8") as message:
@@ -20,6 +21,9 @@ for show in shows:
     try:
         with open(f"tracklists/{show['iso_date']}.json", mode="r") as file:
             tracklist = json.load(file)
+            for song in tracklist:
+                song['date'] = show['iso_date']
+                ALL_SONGS.append(song)
     except Exception as e:
         print("Something went wrong opening tracklist")
         print(e)
@@ -27,4 +31,8 @@ for show in shows:
 
     content = show_template.render(show=show, tracklist=tracklist)
     with open(f"docs/{show['iso_date']}.html", mode="w", encoding="utf-8") as file:
+        file.write(content)
+
+    content = archive_template.render(ALL_SONGS=ALL_SONGS)
+    with open(f"docs/archive.html", mode="w", encoding="utf-8") as file:
         file.write(content)
