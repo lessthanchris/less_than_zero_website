557aea06f4bc214920ed1d5d29739da4d01090a3
Chris Weaver <chris0030@pm.me>
2024-07-12 08:08:09 +0100

    Add July shows and release calendar

--- app.py
+++ app.py
@@ -6,16 +6,22 @@ import json
 with open('shows.json', 'r') as file:
     shows = json.load(file)
 
+with open('releases.json', 'r') as file:
+    releases = json.load(file)
+
 environment = Environment(loader=FileSystemLoader("templates/"), trim_blocks=True, lstrip_blocks=True)
 template = environment.get_template("index.html")
 show_template = environment.get_template("show.html")
 archive_template = environment.get_template("archive.html")
+calendar_template = environment.get_template("calendar.html")
 
 ALL_SONGS = []
 
 content = template.render(shows=shows)
 with open('docs/index.html', mode="w", encoding="utf-8") as message:
     message.write(content)
+
+content = template.render
 
 for show in shows:
     try:
@@ -33,6 +39,10 @@ for show in shows:
     with open(f"docs/{show['iso_date']}.html", mode="w", encoding="utf-8") as file:
         file.write(content)
 
-    content = archive_template.render(ALL_SONGS=ALL_SONGS)
-    with open(f"docs/archive.html", mode="w", encoding="utf-8") as file:
-        file.write(content)
+content = archive_template.render(ALL_SONGS=ALL_SONGS)
+with open(f"docs/archive.html", mode="w", encoding="utf-8") as file:
+    file.write(content)
+
+content = calendar_template.render(releases=releases)
+with open(f"docs/calendar.html", mode="w", encoding="utf-8") as file:
+    file.write(content)
