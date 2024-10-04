affa4edbfcf650c0dee72d9e58320ba7239715e2
Chris Weaver <chris0030@pm.me>
2024-10-04 08:13:17 +0100

    Add 3rd October Show

--- prepare_assets.sh
+++ prepare_assets.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Preparing show assets"
-
+mv $1.png docs/images/
 convert -resize 500x500 docs/images/$1.png docs/images/$1.thumbnail.png
 cwebp docs/images/$1.png -o docs/images/$1.png.webp -resize 500 0 -q 100
 python3 readable_to_json.py $1.txt
