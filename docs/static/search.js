dfb39434f5503aaca0da1d8970d3ee12e331e4c3
Chris Weaver <chris0030@pm.me>
2024-11-29 08:04:47 +0000

    Add song counter

--- docs/static/search.js
+++ docs/static/search.js
@@ -1,10 +1,13 @@
 function search() {
     // Declare variables
     var input, filter, ul, li, a, i, txtValue;
+    var count = 0;
+    var count_display;
     input = document.getElementById('search_box');
     filter = input.value.toUpperCase();
     ul = document.getElementById("song_list");
     li = ul.getElementsByTagName('li');
+    count_display = document.getElementById("songs_found");
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
@@ -12,8 +15,10 @@ function search() {
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
+            count += 1;
         } else {
             li[i].style.display = "none";
         }
     }
+    count_display.innerText = count;
 }
